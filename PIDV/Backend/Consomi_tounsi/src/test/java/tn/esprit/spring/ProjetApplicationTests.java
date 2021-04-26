f03f951332d21841773bf2d8f34df982189273aa
siwar20 <siwar.berzouga@esprit.tn>
2021-04-26 02:30:26 +0100

    frontend

--- PIDV/Backend/Consomi_tounsi/src/test/java/tn/esprit/spring/ProjetApplicationTests.java
+++ PIDV/Backend/Consomi_tounsi/src/test/java/tn/esprit/spring/ProjetApplicationTests.java
@@ -2,7 +2,7 @@ package tn.esprit.spring;
 
 
 //import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,7 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjetApplicationTests {
 	
